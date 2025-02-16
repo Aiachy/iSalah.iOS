40198c05adcee6efb83c611c29974a6b552f62a6
Mert Türedü <nomotetes.onetrue@icloud.com>
2025-02-16 09:42:48 +0300

    xcode init

---
+++ iSalah/iSalah/iSalahApp.swift
@@ -0,0 +1,20 @@
+//
+//  iSalahApp.swift
+//  iSalah
+//
+//  Created by Mert Türedü on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct iSalahApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ iSalah/iSalahTests/iSalahTests.swift
@@ -0,0 +1,17 @@
+//
+//  iSalahTests.swift
+//  iSalahTests
+//
+//  Created by Mert Türedü on 16.02.2025.
+//
+
+import Testing
+@testable import iSalah
+
+struct iSalahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
