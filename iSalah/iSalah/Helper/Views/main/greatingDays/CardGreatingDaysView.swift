1d80c9c47f22d2e5630dc565475eebbea4760796
Mert Türedü <nomotetes.onetrue@icloud.com>
2025-03-13 19:58:31 +0300

    feat: some greating fix

--- iSalah/iSalah/Helper/Views/main/greatingDays/CardGreatingDaysView.swift
+++ iSalah/iSalah/Helper/Views/main/greatingDays/CardGreatingDaysView.swift
@@ -26,7 +26,9 @@ struct CardGreatingDaysView: View {
         backgroundView
             .overlay(alignment: .center) {
                 VStack {
+                    /// Header
                     headerView
+                    /// Content Cards
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack {
                             ForEach(models, id: \.id) { model in
@@ -64,7 +66,7 @@ private extension CardGreatingDaysView  {
             RoundedRectangle(cornerRadius: corner)
                 .stroke(ColorHandler.getColor(salah, for: .islam))
         }
-            .frame(width: size9, height: dh(0.2))
+            .frame(width: size9, height: dh(0.17))
     }
     
     var headerView: some View {
@@ -85,22 +87,43 @@ private extension CardGreatingDaysView  {
         ZStack {
             RoundedRectangle(cornerRadius: 12)
                 .foregroundStyle(ColorHandler.getColor(salah, for: .light))
+                .opacity(0.95)
             VStack(alignment: .leading, spacing: 10) {
                 /// Titles
                 Text(model.name)
                     .font(FontHandler.setNewYorkFont(weight: .bold, size: .S))
-                    .lineLimit(2)
-                    .scaledToFill()
                 
                 /// Date
                 Text(model.date.toFormatted("dd MMMM yyyy"))
                     .font(FontHandler.setNewYorkFont(weight: .bold, size: .xS))
-                    .foregroundStyle(ColorHandler.getColor(salah, for: .shadow))
                 Spacer()
             }
             .frame(width: dw(0.46),alignment: .leading)
+            .foregroundStyle(ColorHandler.getColor(salah, for: .shadow))
             .padding(.vertical,5)
         }
-        .frame(width: dw(0.49), height: dh(0.13), alignment: .leading)
+        .frame(width: dw(0.49), height: dh(0.11), alignment: .leading)
+        .overlay(alignment: .bottomTrailing) { makeCardCounterView(model) }
+    }
+    
+    func makeCardCounterView(_ model: GreatingDaysModel) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .foregroundStyle(ColorHandler.getColor(salah, for: .light))
+                
+            RoundedRectangle(cornerRadius: 4)
+                .stroke(ColorHandler.getColor(salah, for: .shadow))
+                
+            VStack(alignment: .center, spacing: 0) {
+                Text(model.untilDay)
+                    .font(FontHandler.setNewYorkFont(weight: .black, size: .xs))
+                Text("Day")
+                    .font(FontHandler.setDubaiFont(weight: .medium, size: .xxs))
+            }
+            .foregroundStyle(ColorHandler.getColor(salah, for: .shadow))
+                
+        }
+        .frame(width: dw(0.07), height: dh(0.04),alignment: .center)
+        .padding(5)
     }
 }
